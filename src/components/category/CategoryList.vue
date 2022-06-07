5c808b244f7b36f23cba70b421c6dda2ba14fe2a
PC0GF34A-TO\alessio.garbi <alessio.garbi@agmsolutions.net>
2022-06-07 16:54:05 +0200

    lista categorie

--- src/components/category/CategoryList.vue
+++ src/components/category/CategoryList.vue
@@ -1,16 +1,45 @@
 <template>
-    <div>ciao</div>
+  <v-data-table
+    :headers="headers"
+    :items="categories"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
 </template>
 
 
 
 <script>
-
+import { mapGetters,mapActions } from "vuex";
 
 export default ({
    data () {
-      return "";
-   }
+      return {
+         headers: [
+          {
+            text: 'Id',
+            align: 'start',
+            sortable: false,
+            value: 'id',
+          },
+          { text: 'Nome', value: 'name' },
+          { text: 'Descrizione', value: 'description' }
+        ]
+      };
+   },
+   beforeMount() {
+      this.fetchCategorie();
+   },
+   computed:{
+   ...mapGetters({
+      'categories' : 'category/getCategories'
+   })
+   },
+   methods: { 
+      ...mapActions({
+      'fetchCategorie' : 'category/fetchCategories'
+   })}
+  
 })
 
 
