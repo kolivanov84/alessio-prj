fedd922dfff57afa46c7ac9f5bf582cc9ed69d05
PC0GF34A-TO\alessio.garbi <alessio.garbi@agmsolutions.net>
2022-06-08 16:17:06 +0200

    beautify table

--- src/components/category/CategoryList.vue
+++ src/components/category/CategoryList.vue
@@ -1,10 +1,28 @@
 <template>
+   <div>
+   <v-card>
+      <v-card-title>
+         Categorie
+         <v-spacer></v-spacer>
+         <v-text-field
+         v-model="search"
+         append-icon="mdi-magnify"
+         label="Search"
+         single-line
+         hide-details
+         ></v-text-field>
+      </v-card-title>
+   </v-card>
   <v-data-table
     :headers="headers"
+    :sort-by="['name', 'description']"
     :items="categories"
     :items-per-page="5"
     class="elevation-1"
+    :search="search"
+    multi-sort
   ></v-data-table>
+  </div>
 </template>
 
 
@@ -15,15 +33,17 @@ import { mapGetters,mapActions } from "vuex";
 export default ({
    data () {
       return {
+         search: '',
          headers: [
           {
             text: 'Id',
             align: 'start',
-            sortable: false,
+           
             value: 'id',
           },
           { text: 'Nome', value: 'name' },
-          { text: 'Descrizione', value: 'description' }
+          { text: 'Descrizione', value: 'description' },
+          { text: 'Actions', value: 'actions', sortable: false },
         ]
       };
    },
